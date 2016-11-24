0ec6ecc90361f6bc05ac3cb022bf21692e5c3a93
slashhuang <627284482@qq.com>
2016-11-24 14:50:56 +0800

    init rxjs repo with old redux repo

--- example/example.js
+++ example/example.js
@@ -1,76 +1,53 @@
 /**
- * Created by slashhuang on 16/3/8.
+ *  created by slashhuang 11.24
  */
+let Rx = require('rx');
+let $ = require('jquery');
+const $input = $('#input');
+const $results = $('#results');
+
+/* Only get the value from each key up */
+var keyups = Rx.Observable.fromEvent($input, 'keyup')
+  .pluck('target', 'value')
+  .filter(text => text.length > 2 );
+
+/* Now debounce the input for 500ms */
+var debounced = keyups
+  .debounce(500 /* ms */);
+
+/* Now get only distinct values, so we eliminate the arrows and other control characters */
+var distinct = debounced
+  .distinctUntilChanged();
 
 
-
-/**
- * 记录所有被发起的 action 以及产生的新的 state。
- */
-
-/**
- * middleware
- * dispatch
- * =>function(){}
- * =>
- */
-const logger = store => next => action => {
-
-        let result = next(action)
-
-        return result
+function searchWikipedia (term) {
+  return $.ajax({
+    url: 'https://en.wikipedia.org/w/api.php',
+    dataType: 'jsonp',
+    data: {
+      action: 'opensearch',
+      format: 'json',
+      search: term
+    }
+  }).promise();
 };
-const logger1 = store => next => action => {
-        let result = next(action)
-        console.dir(result);
-        return result
-};
-const thunkMiddleware= store => next => action => {
-        let {dispatch,getState}=store;
-                        if (typeof action == 'function') {
-                                return action(dispatch, getState);
-                        }
 
-                        return next(action);
-                };
+let suggestions = distinct.flatMapLatest(searchWikipedia);
+suggestions.subscribe(
+  data => {
+    $results
+      .empty()
+      .append($.map(data[1], value =>  $('<li>').text(value)))
+  },
+  error => {
+    $results
+      .empty()
+      .append($('<li>'))
+        .text('Error:' + error);
+});
 
-/**
- * 引入改写过的store
- */
 
-import todoApp from './reducers';
-import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters,thunkTest } from './actions';
-/**
- * 所有接口放在src/index.js
- */
-import { createStore, combineReducers, applyMiddleware } from '../src/index.js'
 
-let createStoreWithMiddleware = applyMiddleware(logger,logger1,thunkMiddleware)(createStore(todoApp))
-let store = createStoreWithMiddleware;
 
-/**
- * 监听 state 更新时，打印日志
- *
- * 注意 subscribe() 返回一个函数用来注销监听器
- */
-let unsubscribe = store.subscribe(() =>
-        document.body.innerHTML  += JSON.stringify(store.getState())
-);
-let unsubscribe1 = store.subscribe(() =>
-        document.body.innerHTML  += 'subscribing'
-);
 
-/**
- * 发起一系列 action
- */
-store.dispatch(addTodo('Learn about actions'));
-store.dispatch(addTodo('Learn about reducers'));
-store.dispatch(addTodo('Learn about store'));
-store.dispatch(completeTodo(0));
-store.dispatch(completeTodo(1));
-store.dispatch(thunkTest({type:'thunk',text:'thunk'}));
-//unsubscribe1();
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
 
-// 停止监听 state 更新
-unsubscribe();
