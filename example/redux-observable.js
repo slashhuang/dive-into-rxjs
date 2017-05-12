9d5c830ee5a43bddc56bb2cc269af736475a29e4
slashhuang <slashhuang@ali-186590d3234b.local>
2017-05-12 18:12:52 +0800

    uddate epic

--- example/redux-observable.js
+++ example/redux-observable.js
@@ -20,7 +20,7 @@ const epic1 = action$ =>{
   return action$.ofType('epic1')
     .delay(2000)
     .mergeMap(() => Observable.merge(
-      Observable.of(accessDenied()),
+      Observable.of({ type: 'not good'}),
       Observable.timer(2000)
         .mapTo({type:'epic2'})
     ));
@@ -28,20 +28,25 @@ const epic1 = action$ =>{
 const epic2 = action$ =>
   action$.ofType('epic2')
     .delay(1000)
-    .mapTo({ type: 'epic1' });
+    .mapTo({ type: 'epic3' });
 
 const epicMiddleware = createEpicMiddleware(combineEpics(epic1,epic2))
+const logMiddleware = store=>next=>action=>{
+	console.log('logger---',action)
+	next(action)
+}
 const rootReducer = (state={init:'init'}, action) => {
+	debugger;
   switch (action.type) {
-    case 'epic2':return 
-      return { epic2: 'epic2' };
+  	case 'epic3': 
+    case 'epic2': 
     case 'epic1':
-      return { epic1: 'epic1' };
+      return action ;
     default:
       return state;
   }
 };
-const store = createStore(rootReducer,applyMiddleware(epicMiddleware))
+const store = createStore(rootReducer,applyMiddleware(epicMiddleware,logMiddleware))
 
 store.dispatch({type:'epic1'})
 console.log(store.getState())
